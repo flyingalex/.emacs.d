28e6242c4b056c67bdd663698318a06eb140528e
Hu Lin <lin@it-consultis.com>
2016-08-13 18:44:43 +0800

    merge

--- init.el
+++ init.el
@@ -1,5 +1,7 @@
 (global-set-key (kbd "C-z") 'set-mark-command)
-;; package.el 相關設定
+(global-linum-mode t)
+
+;; package.el 相关设置
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
@@ -7,10 +9,25 @@
 
 ;;load path
 (add-to-list 'load-path (locate-user-emacs-file "rc"))
-(add-to-list 'load-path (locate-user-emacs-file "elpa/neotree-20160802.51"))
 
 ;; Generic Modes
 (require 'rc-diredplus)
-
 (require 'neotree)
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(setq web-mode-engines-alist
+      '(("php"    . "\\.phtml\\'")
+        ("blade"  . "\\.blade\\."))
+)
+
+
 (global-set-key [f8] 'neotree-toggle)
+(add-hook 'after-init-hook #'global-flycheck-mode)
